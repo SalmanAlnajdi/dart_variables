f2736cf64d6585e2d54f3478555ddd50385a43df
SalmanAlnajdi <salman.it095@gmail.com>
2022-04-11 04:25:28 +0300

    done

---
+++ .vscode/variables.dart
@@ -0,0 +1,9 @@
+void main() {
+  String firstName = "Salman";
+  String lastName = "Alnajdi";
+  int age = 27;
+  double height = 1.57;
+  bool married = true;
+
+  print("$firstName \n $lastName \n $age  \n $height \n $married ");
+}
